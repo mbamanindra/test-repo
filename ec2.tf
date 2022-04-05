f926fd18a4850f5ebc1fbf2a9e9e0b29597560a6
mbamanindra <99243021+mbamanindra@users.noreply.github.com>
2022-04-05 12:35:30 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-southeast-1"
 }
 
 resource "aws_instance" "test" {
-  ami           = "ami-04893cdb768d0f9ee"
+  ami           = "ami-0553264457fca256a"
   instance_type = "t2.micro"
   tags = {
     Name = "PROD-SERVER"
