399506081221eab770658794d26dadcfa7f396c0
mbamanindra <99243021+mbamanindra@users.noreply.github.com>
2022-04-06 13:19:38 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region     = "ap-southeast-1"
 }
 
-resource "aws_instance" "test" {
+resource "aws_instance" "testing" {
   ami           = "ami-0801a1e12f4a9ccc0"
   instance_type = "t2.medium"
   tags = {
