abec485bb9aed5170894950d8784bfa95c94f2ee
mbamanindra <99243021+mbamanindra@users.noreply.github.com>
2022-04-06 11:16:55 +0530

    Delete s3.tf

--- s3.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "manindra-jenkins"
-    key    = "test/terraform.tfstate"
-    region = "ap-southeast-1"
-     dynamodb_table = "mydynamodb-test-1212"
-
-    }
-}
