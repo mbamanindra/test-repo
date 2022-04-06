2b68a9acfcc0e0a566850bf536df3d09d52ecbbe
mbamanindra <99243021+mbamanindra@users.noreply.github.com>
2022-04-06 11:32:26 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "test" {
   ami           = "ami-0801a1e12f4a9ccc0"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   tags = {
     Name = "PROD-SERVER"
   }
